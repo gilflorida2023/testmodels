e05f1e5ff272081c504d57eeca28cc00b0712483
gil <gilflorida2023@gmail.com>
2025-06-28 00:56:18 -0500

    modified the test file. to execute nearly all in the script.

--- tests.sh
+++ tests.sh
@@ -1,18 +1,22 @@
 rm *.csv *.json *.log 2>/dev/null
 export repetitions=1
-export INCLUDELIST=qwen,gemma,granite
+export INCLUDELIST=qwen,gemma,llava
 #export IGNORELIST=deepcoder,deepscaler,deepseek-r1:1.5b,deepseek-r1:7b,deepseek-r1:32b,deepseek-r1:14b,devstral:24b,granite3.3:2b-base,granite3.3:8b-base,llama3:latest,magistral:24b,minicpm-v:8b,phi4-mini-reasoning:3.8b,phi4-reasoning:latest,qwen2.5vl:3b,qwen2.5vl:7b,qwen2,qwen3:0.6b,qwq:latest
 export IGNORELIST=ibm,gemma3:1b
-python3 modelping.py "How many r's in 'strawberry'?" -s numeric -t 120  -r "$repetitions" -exp 3 -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "How many a's in 'banana'?" -s numeric -t 120  -r "$repetitions" -exp 3 -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "how many e's in 'elephant trunk'?" -s numeric -t 120 -exp 2 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "How many uppercase B's in 'BaBble'?" -s numeric -t 120 -exp 2 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "Count the number of words in 'The quick brown fox'?" -s numeric -t 120 -exp 4 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "Count the number of vowels in 'audio'?" -s numeric -t 120 -exp 4 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "Count the number of consonants in 'rhombus'?" -s numeric -t 120 -exp 5 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "How many words in the sentence begins with r: 'The rooster runs fast'?" -s numeric -t 120 -exp 2 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
-python3 modelping.py "How many letters in 'dog' + 'cat'? " -s numeric -t 180 -exp 6 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
-
+#python3 modelping.py "How many r's in 'strawberry'?" -s numeric -t 120  -r "$repetitions" -exp 3 -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "How many a's in 'banana'?" -s numeric -t 120  -r "$repetitions" -exp 3 -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "how many e's in 'elephant trunk'?" -s numeric -t 120 -exp 2 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "How many uppercase B's in 'BaBble'?" -s numeric -t 120 -exp 2 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "Count the number of words in 'The quick brown fox'?" -s numeric -t 120 -exp 4 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "Count the number of vowels in 'audio'?" -s numeric -t 120 -exp 4 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "Count the number of consonants in 'rhombus'?" -s numeric -t 120 -exp 5 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "How many words in the sentence begins with r: 'The rooster runs fast'?" -s numeric -t 120 -exp 2 -r "$repetitions" -e "$IGNORELIST" -i "$INCLUDELIST"
+#python3 modelping.py "How many words in the sentence 'The rat runs fast.' begin with  'r'?" -s numeric -t 120 -exp 2  -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
+#
+#
+#
+#python3 modelping.py "How many letters in 'dog' + 'cat'? " -s numeric -t 180 -exp 6 -r "$repetitions"  -e "$IGNORELIST" -i "$INCLUDELIST"
+#
 python3 modelping.py "What is 9 × 5? Answer with a number." -s numeric -t 120 -exp 45 -r "$repetitions" -i "$INCLUDELIST"
 python3 modelping.py "If 20 items are split equally among 5 people, how many does each get? Answer with a number." -s numeric -t 120 -exp 4 -r "$repetitions" -i "$INCLUDELIST"
 python3 modelping.py "What is 15 + 8 + 7? Answer with a number." -s numeric -t 120 -exp 30 -r "$repetitions" -i "$INCLUDELIST"
